c40291890e3e19397e2837bb08725f9088f7b570
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2026-09-18 12:57:45 -0300

    Update .brewfile Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,7 @@
+# The following lines were added by Docker Desktop to add commands to your PATH.
+export PATH="$PATH:/Users/lucasribeiro/.docker/bin"
+# End of Docker Desktop section.
+
 if status is-interactive
 
 set fish_greeting
